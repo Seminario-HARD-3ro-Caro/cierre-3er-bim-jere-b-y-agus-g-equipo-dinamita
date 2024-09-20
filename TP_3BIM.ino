ad62d9a661a1861b524e82f186e9a2f56ed49778
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2024-09-20 10:47:20 -0300

    Update TP_3BIM.ino

--- TP_3BIM.ino
+++ TP_3BIM.ino
@@ -21,8 +21,8 @@ Congisgnas
         LED3: 10
         LED4: 11
       Valor:
-        R_boton: PULL DOWN
-        R_LEDs: 
+        R_boton: PULL UP
+        R_LEDs: 2.6 OHM
 
   Progamas:
     Se deben entregar un programa distinto por cada punto. Los programas son una modificación entre ellos.
